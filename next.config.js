d880c00df87b2daa69595a6664e8e31d4e3b1488
patrick-lowry <pllowry@hotmail.com>
2026-01-09 07:50:04 +0000

    Modified so that Tina starts locally at same time as Next.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  env: {
+    stackbitPreview: process.env.STACKBIT_PREVIEW
+  },
+  trailingSlash: true,
+  reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: "/admin",
+        destination: "/admin/index.html",
+        permanent: false
+      }
+    ];
+  }
+};
+
+module.exports = nextConfig;
+
+
+
+
+/**
 const nextConfig = {
     env: {
         stackbitPreview: process.env.STACKBIT_PREVIEW
@@ -8,3 +33,4 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+*/
